f7cbc75ec2df9e48083b517e32959a65e70dcf51
Jiaxin Zhao <jzhao537@uwo.ca>
2021-02-15 14:17:43 +0800

    commit more pages

---
+++ umall_mobile/src/pages/mine/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>Mine</h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- umall_mobile/src/router/index.js
+++ umall_mobile/src/router/index.js
@@ -11,7 +11,33 @@ const router = new Router({
     },
     {
       path: '/login',
+      meta: {
+        title: '用户登录'
+      },
       component: () => import('@/pages/login/Index')
+    },
+    {
+      path: '/cart',
+      meta: {
+        title: '购物车',
+        checkLogin: true // 是否需要进行登录的判断
+      },
+      component: () => import('@/pages/cart/Index')
+    },
+    {
+      path: '/mine',
+      meta: {
+        title: '我的',
+        checkLogin: true // 是否需要进行登录的判断
+      },
+      component: () => import('@/pages/mine/Index')
+    },
+    {
+      path: '/classification',
+      meta: {
+        title: '商品分类'
+      },
+      component: () => import('@/pages/classification/Index')
     }
   ],
   mode: 'history'
